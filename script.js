11252f1039c46268a51a6dade5121f79fde2c236
Flor <mariaflorenciaperezd@gmail.com>
2025-04-24 20:50:42 +0200

    Archivos creados

---
+++ script.js
@@ -0,0 +1,20 @@
+/*### **2️⃣ Configurar el Frontend**
+
+Recuerda que el repositorio del frontend que tienes que forkear es este: `https://github.com/TheBridge-FullStackDeveloper/books-collection-front`
+
+1. Crea un proyecto con **HTML, CSS y JavaScript**
+2. Usa `fetch` o `axios` para consumir los endpoints de la API REST.
+3. Crea una página y agrega dos botones:
+   - Botón "Mostrar usuarios".
+   - Botón "Mostrar libros".
+
+## 🚀 Bonus
+
+Si terminas antes, intenta agregar:
+
+- Un **loader** mientras se cargan los datos.
+- Manejo de **errores y mensajes de confirmación**.
+- Agrega un botón "Ver Colección" en cada usuario para hacer una petición a `GET /users/:id` y mostrar su colección de libros.
+
+---
+ */
